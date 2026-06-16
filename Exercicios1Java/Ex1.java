4e685818a7fa674e536f635c94c79ff02f6bb92b
juusce <jusce013@gmail.com>
2026-06-16 20:47:34 -0300

    Add files via upload

---
+++ Exercicios1Java/Ex1.java
@@ -0,0 +1,11 @@
+public class Ex1 {
+public static void main(String[] args) {
+        
+    String nome = "João";
+    int idade = 20;
+    
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos.");
+}
+    }
+    
+
